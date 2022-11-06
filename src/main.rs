44f95ec9146a2fc7cb2d07e4476974797ee57891
taochong <taochong@kezaihui.com>
2022-11-06 17:42:29 +0800

    chap 3.2: data types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-fn main() {
-    let spaces = "   ";
-    let spaces = spaces.len();
+use std::io;
 
-    println!("{spaces}")
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index: ");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a valid number.");
+
+    let element = a[index];
+
+    println!("The value of the element at index {} is {}", index, element);
 }
